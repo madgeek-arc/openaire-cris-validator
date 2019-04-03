283ab49984593f9bb10f4787f06c79f7d2af9b55
Konstantinos Spyrou <spyroukon@gmail.com>
2019-04-03 17:33:21 +0300

    created Job class

---
+++ src/main/java/org/eurocris/openaire/cris/validator/Job.java
@@ -0,0 +1,96 @@
+package org.eurocris.openaire.cris.validator;
+
+import java.util.Date;
+import java.util.Map;
+import java.util.UUID;
+
+public class Job {
+
+    private String id;
+    private String url;
+    private String user;
+    private String status;
+
+    private Date dateSubmitted;
+    private Date dateStarted = null;
+    private Date dateFinished = null;
+
+    private Map<String, String> rules = null;
+
+
+    Job() {}
+
+    Job(String url, String user) {
+        this.url = url;
+        this.user = user;
+
+        this.id = UUID.randomUUID().toString();
+        this.status = "pending";
+        this.dateSubmitted = new Date();
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getDateSubmitted() {
+        return dateSubmitted;
+    }
+
+    public void setDateSubmitted(Date dateSubmitted) {
+        this.dateSubmitted = dateSubmitted;
+    }
+
+    public Date getDateStarted() {
+        return dateStarted;
+    }
+
+    public void setDateStarted(Date dateStarted) {
+        this.dateStarted = dateStarted;
+    }
+
+    public Date getDateFinished() {
+        return dateFinished;
+    }
+
+    public void setDateFinished(Date dateFinished) {
+        this.dateFinished = dateFinished;
+    }
+
+    public Map<String, String> getRules() {
+        return rules;
+    }
+
+    public void setRules(Map<String, String> rules) {
+        this.rules = rules;
+    }
+}
